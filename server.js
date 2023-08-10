cb5ba5328621d818f155f786dc46aa8c12f43f3f
Eshita Islam <islam.eshita@gmail.com>
2023-08-09 20:03:29 -0700

    Added a movies route

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
-require("dotenv").config();
+// Dependencies
 const express = require("express");
 const app = express();
+
+// Configuration / Middleware
+require("dotenv").config();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
   res.send("Movie Database Backend Homepage");
 });
+
+// Movie Route
+const moviesController = require("./controllers/movies");
+app.use("/movies", moviesController);
 
 // Listen
 app.listen(process.env.BACKEND_PORT, () => {
