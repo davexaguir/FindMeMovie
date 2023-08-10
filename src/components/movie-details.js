b576d2e73fa1f577662f065a4b97f3c0ef6caec2
dagui <davaguir852@gmail.com>
2023-08-09 18:25:40 -0700

    first commit creating react app

---
+++ src/components/movie-details.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+export default class MovieDetails extends Component {
+  render() {
+    return (
+      <div>
+        <p>React Create movie details Component!</p>
+      </div>
+    );
+  }
+}
---
+++ src/components/movie-list.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+export default class MovieList extends Component {
+  render() {
+    return (
+      <div>
+        <p>React Create movies list Component!</p>
+      </div>
+    );
+  }
+}
