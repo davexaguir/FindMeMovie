4378db04f76925075698769814383553e9265755
Eshita Islam <islam.eshita@gmail.com>
2023-08-14 19:15:58 -0700

    Fixed issue with backend code not working correctly

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,8 @@
 // Dependencies
-require('dotenv').config()
+require("dotenv").config();
 const express = require("express");
 const app = express();
-const mongoose = require ('mongoose')
-
-
-// Configuration / Middleware
-require("dotenv").config();
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+const mongoose = require("mongoose");
 
 app.get("/", (req, res) => {
   res.send("Movie Database Backend Homepage");
@@ -17,11 +11,6 @@ app.get("/", (req, res) => {
 // Movie Route
 const moviesController = require("./controllers/movies");
 app.use("/movies", moviesController);
-
-//mongoose connection
-mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, ()=> {
-   console.log('Databse connected succesfully!')
- })
 
 // Listen
 app.listen(process.env.BACKEND_PORT, () => {
