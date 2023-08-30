3fedcaf90b989486ee9fca3b8779b23053c0bf55
Eshita Islam <islam.eshita@gmail.com>
2023-08-30 00:06:42 -0700

    Fixed issue with CORS

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,11 @@
-//test comment
-// Dependencies
-// Dependencies pushing the renamed branch
 const express = require("express");
+const cors = require("cors");
 const app = express();
 
 require("dotenv").config();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Movie Database Backend Homepage");
